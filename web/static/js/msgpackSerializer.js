a847967f2e53aea737ecc322fd3b177847ca337f
Jonathan Merriweather <Jono@merriweather.com.au>
2016-07-29 15:03:42 +1000

    removed requirement for Promise

--- web/static/js/msgpackSerializer.js
+++ web/static/js/msgpackSerializer.js
@@ -8,23 +8,18 @@ export default class MsgPackSerializer {
 
   binaryType() { return "arraybuffer" }  
 
-  encode(payload) {
-    return new Promise((resolve) => {
-      resolve(msgpack.encode(payload))
-    })
+  encode(payload, callback) {
+    callback(msgpack.encode(payload))
   }
 
-  decode(payload) {
-    console.log(payload)
-    return new Promise((resolve) => {
-      var fileReader = new FileReader()
-      fileReader.onload = function(e) {
-        let bytes = new Uint8Array(e.target.result.slice(0, e.target.result.byteLength))
-        //console.log("about to decode message, that starts with", bytes);
-        let msg = msgpack.decode(bytes)
-        resolve(msg)
-      }
-      fileReader.readAsArrayBuffer(payload)
-    })
+  decode(payload, callback) {
+    var fileReader = new FileReader()
+    fileReader.onload = function(e) {
+      let bytes = new Uint8Array(e.target.result.slice(0, e.target.result.byteLength))
+      //console.log("about to decode message, that starts with", bytes);
+      let msg = msgpack.decode(bytes)        
+      callback(msg)
+    }
+    fileReader.readAsArrayBuffer(payload)
   }
 }
